81485ac3015251bf7533b7f03215981c5ac68181
Natalia Moskvychova <110030591+NaTaLiaMoSKV@users.noreply.github.com>
2022-12-24 16:35:55 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+// console.dir(galleryContainer);
+
+function createGalleryItemsMarkup(items) {
+    return items.map( ({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    }).join('');
+}
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    evt.preventDefault();
+    const originalImageSrc = evt.target.dataset.source;
+    createBasicLightBox(originalImageSrc);
+}
+
+function createBasicLightBox(source) {
+    const instance = basicLightbox.create(
+        `<img class="gallery__image" src=${source}></img>`
+    )
+    instance.show()
+}
